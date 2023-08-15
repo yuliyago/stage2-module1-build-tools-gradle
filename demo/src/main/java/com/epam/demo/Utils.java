87b2e87de2cf7f062adacce91a80c7b046b617bf
yuliyago <yuuliya02@proton.me>
2023-08-15 22:13:47 +0300

    Gradle task was updated.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,19 +1,31 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for (String arg : args) {
             try {
-                int num = Integer.parseInt(arg);
+                double num = Double.parseDouble(arg);
+
                 if (num <= 0) {
                     return false;
                 }
             } catch (NumberFormatException e) {
+                System.out.println("mist");
                 return false;
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        List<String> lst=new ArrayList<>();
+        lst.add("11.22");
+        lst.add("2");
+        lst.add("33");
+        lst.add("87");
+        System.out.println(isAllPositiveNumbers(lst));
     }
 }
