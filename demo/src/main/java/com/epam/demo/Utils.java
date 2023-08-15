6ee3f967060c0b8e562139cf6010ad7ca9323310
yuliyago <yuuliya02@proton.me>
2023-08-15 17:49:52 +0300

    Gradle task has been done.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            try {
+                int num = Integer.parseInt(arg);
+                if (num <= 0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        return org.apache.commons.lang3.StringUtils.isNumeric(str);
     }
 }
